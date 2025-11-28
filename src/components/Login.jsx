d3eceb4b7f49dc0f0e9637cab6a51d688e535acb
Pavan Teja Reddy <142150313+PavanTejaReddy1@users.noreply.github.com>
2025-11-28 20:29:29 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,1 +1,12 @@
 
+export default function Login(isLoggedIn) {
+
+  return (
+    <form onSubmit = (()=>isLoggedIn(true))>
+      <label for="name">Username: </label>
+      <input type="text" id="name"></input>
+      <label for="password">Password: </label>
+      <input typr="password" id="password"></input>
+    </form>
+  )
+}
