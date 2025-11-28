0248f7ab645eceb4d4ea3b70a4dd8383e77a027c
Pavan Teja Reddy <142150313+PavanTejaReddy1@users.noreply.github.com>
2025-11-28 20:31:45 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState } from 'react'
+import Login from "./Login";
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div>
-        {/* Do not remove the main div */}
+    <login setIsLoggedIn={setIsLoggedIn}/>
     </div>
   )
 }
